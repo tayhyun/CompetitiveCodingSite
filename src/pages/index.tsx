522dd4fbddba365f6caaf4c5ac4a3e77d67841b2
HwangJasonJaesung <HWANG_JAE_SUNG@student.smc.edu>
2024-10-18 06:33:40 +0900

    added a line to test push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
      <main>
       <h1>Hello Lucas!</h1>
       <p> lucas's hair is very yummu.</p>
+      <p> Jason is working on the site currently</p>
       <p>hello sexy people I love lucas!!!!</p>
      </main>
     </div>
